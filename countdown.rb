0f2ff9981f4ec0d7c9de6fad4e097fcb00727020
Blaire <grant.blaire@gmail.com>
2020-11-14 23:15:41 -0600

    commit tree a494b9fe7d8b3384950d0caad9a0e8c0943ef7e0

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
-#write your code here
+number = 0
 
-def countdown
+def countdown(number)
+    if number < 12
+        while number > 0  
+            puts "#{number} SECOND(S)!"
+            number -= 1
+        end
+    else "HAPPY NEW YEAR!"
+    end
+ end
+
+number = 0
+
+def countdown_with_sleep(number)
+    while number > 0
+        sleep 1
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
 end
+
+
+
+
